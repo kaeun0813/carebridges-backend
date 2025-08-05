1af6e83fcb499552c0bdc4da4e8d9333b5192a82
kaeun0813 <rk081321@gmail.com>
2025-08-06 01:50:41 +0900

    fix:스크립트 서비스 이름 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 
 # 애플리케이션을 시작하는 명령어
 echo "Starting the FastAPI application using systemd..."
-sudo systemctl start carebridges  # carebridges.service 실행
+sudo systemctl start carebridges.service  # carebridges.service 실행
 echo "FastAPI application started."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,5 +2,5 @@
 
 # 애플리케이션 중지 명령어
 echo "Stopping the FastAPI application using systemd..."
-sudo systemctl stop carebridges  # carebridges.service 중지
+sudo systemctl stop carebridges.service  # carebridges.service 중지
 echo "FastAPI application stopped."
